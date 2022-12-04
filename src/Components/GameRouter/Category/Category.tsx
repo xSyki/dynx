b9ad0348fc3ca7b6bfd7765a7d78bcddfdd14c68
xSyki <mikolajsykula@gmail.com>
2022-12-04 16:07:28 +0100

    Categories filter

--- src/Components/GameRouter/Category/Category.tsx
+++ src/Components/GameRouter/Category/Category.tsx
@@ -2,28 +2,36 @@ import styled from 'styled-components/native'
 
 import images from '../../../../assets/images/images'
 import categories from '../../../../assets/words/categories.json'
+import wordsList from '../../../../assets/words/words.json'
 import i18n, { LanguageEnum } from '../../../i18n/config'
 import {
   GameStatusEnum,
   ICategory,
   useGameStore,
 } from '../../../stores/gameStore'
+import { getRoundLength } from '../../../utils/getRounds'
 import StyledButton from '../../Atoms/StyledButton'
 import StyledText from '../../Atoms/StyledText'
 
 function Category() {
-  const [, { setGameStatus, setCategory }] = useGameStore()
+  const [{ players, gameMode }, { setGameStatus, setCategory }] = useGameStore()
 
   const handleChoseCategory = (category: ICategory) => {
     setCategory(category)
     setGameStatus(GameStatusEnum.SETTINGS)
   }
 
+  const filteredCategories = categories.filter(
+    (category) =>
+      wordsList.filter((word) => word.categories.includes(category.id)).length >
+      getRoundLength(players.length, gameMode)
+  )
+
   return (
     <StyledCategory>
       <StyledText size="bg">Categories</StyledText>
       <StyledButtons>
-        {(categories as ICategory[]).map((category) => (
+        {(filteredCategories as ICategory[]).map((category) => (
           <StyledButton
             onPress={() => handleChoseCategory(category)}
             key={category.id}
